404fb2689dabaa250944e0704153cf306af07d4c
avinash <biradarav.100@gmail.com>
2021-08-06 11:54:03 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,21 @@
+setInterval(() => {
+    
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+
+    hrotate = 30*htime + mtime/2;
+    mrotate = 6*mtime;
+    srotate = 6*stime;
+
+    // console.log(hrotate);
+    // console.log(mrotate);
+    // console.log(srotate);
+    
+
+    hour.style.transform = `rotate(${hrotate}deg)`;
+    minute.style.transform = `rotate(${mrotate}deg)`;
+    second.style.transform = `rotate(${srotate}deg)`;
+
+},1000);
